2ef8097812fc8e655ba6c2ea96afb87413dc0009
Gwen Lg <me@gwenlg.fr>
2025-01-23 23:25:08 +0100

    wip: add file processing management

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 //! `subtools` is a library line app to check and manipulate subtitles.
 //! A command line utilities is also provided.
 
+mod file_processor;
 mod subtitle_file;
 
+pub use file_processor::FileProcessor;
 pub use subtitle_file::SubtitleFile;
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,38 @@
-//! `subtools` is a command line app to check and manipulate subtitles.
+//! A command line utilities to test and use subtools functionalities.
 
-use std::{env, path::PathBuf};
+use anyhow::Context;
+use clap::Parser;
+use std::{env, ffi::OsString, path::PathBuf};
+use subtools::{FileProcessor, SubtitleFile};
 
-use subtools::SubtitleFile;
+/// A CLI application to manipulate subtitles files.
+#[derive(Debug, Parser)]
+#[command(name = "sub_tools")]
+#[command(about = "A command line tool to manipulate subtitles files with help of `subtile`", long_about = None)]
+struct Cli {
+    /// Can be a file, or a folder, if folder, it tried to process all compatible files of the folder.
+    #[arg(short, long, value_name = "PATH")]
+    pub path: Option<OsString>,
+}
 
 fn main() -> anyhow::Result<()> {
-    env::args().for_each(|arg| {
-        let path = PathBuf::from(arg);
-        if !path.exists() {
-            println!("Not a valid path : {path:?}");
-        } else if !path.is_file() {
-            println!("Not a file : {path:?}");
-        } else {
-            match SubtitleFile::try_from(path.as_path()) {
-                Ok(subfile) => {
-                    println!("'{path:?}' is a {}", subfile.format());
-                }
-                Err(err) => {
-                    println!("Arg `{path:?}` is not recognized as subtitle file : {err}");
-                }
+    let args = Cli::parse();
+
+    //TODO: move into a file
+    let in_path = if let Some(path) = args.path {
+        PathBuf::from(path)
+    } else {
+        env::current_dir().context("Failed to access to current directory")?
+    };
+
+    let files_processor = FileProcessor::from_path(in_path);
+    files_processor.subtitle_files().for_each(|file| {
+        match SubtitleFile::try_from(file.as_path()) {
+            Ok(sub_file) => {
+                eprintln!("{file:?} is recognized as {}", sub_file.format());
+            }
+            Err(err) => {
+                eprintln!("{file:?} is not a recognized subtitle file : {err:?}");
             }
         }
     });
