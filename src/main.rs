d0afa04710f9bd0993871d189babb4386e5acd5e
Gwen Lg <me@gwenlg.fr>
2025-01-23 23:25:08 +0100

    wip feat: add files processing management
    FileProcessor is a helper to manage multiple files in a generic way.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,47 @@
 //! A command line utility use subtools functionalities.
 
-use std::{env, path::PathBuf};
-
-use subtools::SubtitleFile;
+use anyhow::Context as _;
+use std::{
+    borrow::Borrow,
+    env,
+    path::{Path, PathBuf},
+};
+use subtools::{FileProcessor, SubtitleFile};
 
 fn main() -> anyhow::Result<()> {
-    env::args().skip(1).for_each(|arg| {
-        let path = PathBuf::from(arg);
+    let paths = if env::args().len() <= 1 {
+        let local_path = env::current_dir().context("Failed to access to current directory")?;
+        vec![local_path]
+    } else {
+        env::args().skip(1).map(PathBuf::from).collect::<Vec<_>>()
+    };
+
+    paths.into_iter().for_each(|path| {
         if !path.exists() {
             println!("Not a valid path : {path:?}");
-        } else if !path.is_file() {
-            println!("Not a file : {path:?}");
-        } else {
-            match SubtitleFile::try_from(path.as_path()) {
-                Ok(subfile) => {
-                    println!("{path:?} is a {}", subfile.format());
-                }
-                Err(err) => {
-                    println!("Arg {path:?} is not recognized as subtitle file : {err}");
-                }
-            }
+        } else if path.is_file() {
+            check_file(path);
+        } else if path.is_dir() {
+            let files_processor = FileProcessor::from_path(path);
+            files_processor.subtitle_files().for_each(|file| {
+                check_file(file);
+            });
         }
     });
     Ok(())
+}
+
+fn check_file<F>(file: F)
+where
+    F: Borrow<Path>,
+{
+    let file = file.borrow();
+    match SubtitleFile::try_from(file) {
+        Ok(subfile) => {
+            eprintln!("{file:?} is recognized as {}", subfile.format());
+        }
+        Err(err) => {
+            eprintln!("{file:?} is not a recognized subtitle file : {err:?}");
+        }
+    }
 }
