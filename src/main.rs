8f10974f2b36fdf456ecd0a654580844e59f85fa
Gwen Lg <me@gwenlg.fr>
2025-01-23 23:25:08 +0100

    feat(cli): add clap to manage cli app

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,31 @@
 //! A command line utility use subtools functionalities.
 
 use anyhow::Context as _;
+use clap::Parser;
 use std::{
     borrow::Borrow,
     env,
+    ffi::OsString,
     path::{Path, PathBuf},
 };
 use subtools::{FileProcessor, SubtitleFile};
 
-fn main() -> anyhow::Result<()> {
-    let paths = if env::args().len() <= 1 {
-        let local_path = env::current_dir().context("Failed to access to current directory")?;
-        vec![local_path]
-    } else {
-        env::args().skip(1).map(PathBuf::from).collect::<Vec<_>>()
-    };
+/// A CLI application to manipulate subtitles files.
+#[derive(Debug, Parser)]
+#[command(name = "sub_tools")]
+#[command(about = "A command line tool to manipulate subtitles files with help of `subtile`", long_about = None)]
+struct Cli {
+    /// Can be a file, or a folder, if folder, it tried to process all compatible files of the folder.
+    #[arg(short, long, value_name = "PATH")]
+    pub path: Option<OsString>,
+}
 
-    paths.into_iter().for_each(|path| {
-        if !path.exists() {
-            println!("Not a valid path : {path:?}");
-        } else if path.is_file() {
-            check_file(path);
-        } else if path.is_dir() {
-            let files_processor = FileProcessor::from_path(path);
-            files_processor.subtitle_files().for_each(|file| {
-                check_file(file);
-            });
-        }
-    });
+fn main() -> anyhow::Result<()> {
+    let args = Cli::parse();
+
+    let working_path = get_working_path(&args)?;
+    let files_processor = FileProcessor::from_path(working_path);
+    files_processor.subtitle_files().for_each(check_file);
     Ok(())
 }
 
@@ -44,4 +42,14 @@ where
             eprintln!("{file:?} is not a recognized subtitle file : {err:?}");
         }
     }
+}
+
+// get a working path from args or current dir.
+fn get_working_path(args: &Cli) -> Result<PathBuf, anyhow::Error> {
+    let in_path = if let Some(path) = &args.path {
+        PathBuf::from(path)
+    } else {
+        env::current_dir().context("Failed to access to current directory")?
+    };
+    Ok(in_path)
 }
