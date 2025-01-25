2cc45eb68d73750e529c78f76345effd5a898c7a
Gwen Lg <me@gwenlg.fr>
2025-01-25 23:44:33 +0100

    feat: add subtitle_file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
-//! `subtools` is a command line app to check and manipulate subtitles.
+//! A command line utility use subtools functionalities.
+
+use std::{env, path::PathBuf};
+
+use subtools::SubtitleFile;
 
 fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    env::args().skip(1).for_each(|arg| {
+        let path = PathBuf::from(arg);
+        if !path.exists() {
+            println!("Not a valid path : {path:?}");
+        } else if !path.is_file() {
+            println!("Not a file : {path:?}");
+        } else {
+            match SubtitleFile::try_from(path.as_path()) {
+                Ok(subfile) => {
+                    println!("{path:?} is a {}", subfile.format());
+                }
+                Err(err) => {
+                    println!("Arg {path:?} is not recognized as subtitle file : {err}");
+                }
+            }
+        }
+    });
     Ok(())
 }
