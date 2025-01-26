e8a16ab0e70d1508c4f2ea98bb96be1496f4b928
Gwen Lg <me@gwenlg.fr>
2025-01-26 21:05:32 +0100

    rework convert_to_utf-8

--- src/subtitle_file.rs
+++ src/subtitle_file.rs
@@ -81,6 +81,15 @@ impl SubtitleFile {
     pub const fn format(&self) -> SubtitleFormat {
         self.format
     }
+
+    pub fn gen_new_name(&self, pre_ext: &str) -> PathBuf {
+        //TODO: manage lang separate with `.`
+        //TODO: keep
+        let file_stem = self.path.file_stem().unwrap().to_str().unwrap().to_string();
+        let ext = self.path.extension().unwrap().to_str().unwrap();
+        let new_filename = format!("{file_stem}.{pre_ext}.{ext}");
+        self.path.with_file_name(new_filename)
+    }
 }
 
 impl<'a> TryFrom<&'a Path> for SubtitleFile {
