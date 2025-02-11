c03966933d51d2eb3603e33690235121138ea6a3
Gwen Lg <me@gwenlg.fr>
2025-02-11 21:55:34 +0100

    wip: add trait `FrameHandler`

--- src/matroska/mod.rs
+++ src/matroska/mod.rs
@@ -3,3 +3,9 @@
 mod codec_id;
 
 pub use codec_id::CodecId;
+
+/// Define the interface to handle a frame from source (like matroska)
+pub trait FrameHandler {
+    /// Call with information/data on a subtitle line.
+    fn push_frame(&mut self, timestamp: u64, duration: Option<u64>, content: &[u8]);
+}
