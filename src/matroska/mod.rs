c4c048b3d100bb22661a12cbe6563d60e90bdd6c
Gwen Lg <me@gwenlg.fr>
2025-02-11 21:55:34 +0100

    wip: add trait `SubtitleLineDecoder`

--- src/matroska/mod.rs
+++ src/matroska/mod.rs
@@ -3,3 +3,10 @@
 mod codec_id;
 
 pub use codec_id::CodecId;
+use subtile::time::TimeSpan;
+
+/// Define the interface for manage (decode) a subtitle line from source (like matroska)
+pub trait SubtitleLineDecoder {
+    /// Call with information/data on a subtitle line.
+    fn push_sub_line(&mut self, time: TimeSpan, content: &[u8]);
+}
