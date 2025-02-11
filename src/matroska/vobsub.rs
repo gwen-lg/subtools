d402c814f71a6c6f8fef353f780d5e54ed30b517
Gwen Lg <me@gwenlg.fr>
2025-02-11 22:01:22 +0100

    wip: add a vobsub decoder

---
+++ src/matroska/vobsub.rs
@@ -0,0 +1,24 @@
+use std::str;
+
+use super::SubtitleLineDecoder;
+use subtile::time::TimePoint;
+
+///Wip + TODO
+pub struct VobSubDecoder {}
+impl VobSubDecoder {
+    /// Create a `VobSub` subtitle line Decoder.
+    #[must_use]
+    #[allow(clippy::missing_panics_doc)] //TODO: remove unwrap by return error, or by get str data
+    pub fn new(index_header: &[u8]) -> Self {
+        let index_header = str::from_utf8(index_header).unwrap();
+        println!("VobSub idx:{index_header}");
+        Self {}
+    }
+}
+impl SubtitleLineDecoder for VobSubDecoder {
+    fn push_sub_line(&mut self, timestamp: u64, _duration: Option<u64>, content: &[u8]) {
+        let time_point = TimePoint::from_msecs(timestamp as i64);
+        let content_size = content.len();
+        println!("VobSub frame `{}`: {content_size}", time_point.to_secs(),);
+    }
+}
