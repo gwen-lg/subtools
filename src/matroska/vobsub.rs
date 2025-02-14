3b595bdd41ec6afa18b8b24cb367e9c3a7630a43
Gwen Lg <me@gwenlg.fr>
2025-02-14 18:06:20 +0100

    wip: tried to extract vobsub from mkv implementation

--- src/matroska/vobsub.rs
+++ src/matroska/vobsub.rs
@@ -41,6 +41,11 @@ where
         );
         self.sub_writer.write_all(content).unwrap();
 
+        let ps_header = MpegPsHeader::new(time_point.msecs() as u64);
+
+        //self.sub_writer.wri
+        ps_header.write(&mut self.writer).unwrap();
+
         let filepos = 0; //TODO self.sub_writer
 
         // filepos: 000000000
@@ -51,4 +56,40 @@ where
         )
         .unwrap();
     }
+}
+
+// WIP
+// http://dvdnav.mplayerhq.hu/dvdinfo/mpeghdrs.html
+//const MPEG_PS_TAG: u32 = 0x0000_01ba; //[0x00, 0x00, 0x01, 0xba]
+
+#[derive(BinWrite)]
+#[bw(little, magic = 0x0000_01bau32)] //TODO: Little of big endian ?
+struct MpegPsHeader {
+    #[bw(align_before = 0x40)]
+    scr: [u8; 6],
+
+    #[bw()]
+    muxr: [u8; 3],
+
+    #[bw(value = 0xf8)]
+    stlen: u8,
+}
+
+impl MpegPsHeader {
+    fn new(time: u64) -> Self {
+        let mut src = [0; 6];
+        src[0] = ;
+        src[1] = 1;
+        Self {
+            scr,
+            muxr: [0x01, 0x89, 0xc3],
+            stlen: 0xf8,
+        }
+    }
+}
+
+#[derive(BinWrite)]
+#[bw(little, magic = b"\x00\x00\x01")] //TODO: Little of big endian ?
+struct MpegEsHeader {
+    pfx: [u8; 3],
 }
