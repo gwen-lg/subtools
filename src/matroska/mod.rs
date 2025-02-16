c6460d4246e8aa3100bdc3c13137ca94c667a1ba
Gwen Lg <me@gwenlg.fr>
2025-02-16 13:14:57 +0100

    wip: add Frame time to TimeSpan fn convertion

--- src/matroska/mod.rs
+++ src/matroska/mod.rs
@@ -10,10 +10,19 @@ pub use subrip::SrtWriter;
 pub use vobsub::VobSubDecoder;
 pub use webvtt::WebvttWriter;
 
-use subtile::time::TimeSpan;
+use subtile::time::{TimePoint, TimeSpan};
 
 /// Define the interface for manage (decode) a subtitle line from source (like matroska)
 pub trait SubtitleLineDecoder {
     /// Call with information/data on a subtitle line.
     fn push_sub_line(&mut self, time: TimeSpan, content: &[u8]);
+}
+
+/// Create a `TimeSpan` from the frame timestamp and a duration
+#[must_use]
+pub const fn frame_time_span(timestamp: u64, duration: u64) -> TimeSpan {
+    assert!(timestamp <= i64::MAX as u64); //TODO: convert to Error
+    let time_start = TimePoint::from_msecs(timestamp as i64);
+    let time_end = TimePoint::from_msecs((timestamp + duration) as i64);
+    TimeSpan::new(time_start, time_end)
 }
