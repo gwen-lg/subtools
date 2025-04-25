ac721bfeecda2b43b63627f0e8b48284ee47a823
Gwen Lg <me@gwenlg.fr>
2025-04-25 18:21:28 +0200

    wip: test use fixup instead of simple check

--- src/check.rs
+++ src/check.rs
@@ -1,7 +1,7 @@
 use crate::{FileProcessor, IterProcessing, ProcessingContext, SubProcess, SubtitleFile};
 use regex::Regex;
-use srtlib::{ParsingError, Subtitles};
-use std::{fmt::Display, path::PathBuf, sync::LazyLock};
+use srtlib::{ParsingError, Subtitle, Subtitles};
+use std::{cell::RefCell, fmt::Display, path::PathBuf, rc::Rc, sync::LazyLock};
 use thiserror::Error;
 
 type ReportEntry = String;
@@ -46,6 +46,13 @@ enum CheckError {
 
     #[error("some correction can be done in subtitle : \n{0}")]
     Report(Report),
+
+    #[error("failed to write to file `{file}' as srt/subrip format")]
+    WriteToFile {
+        #[source]
+        source: ParsingError,
+        file: PathBuf,
+    },
 }
 
 /// Check text subtitles with predefined and custom rules.
@@ -56,9 +63,16 @@ pub fn check_subs(proc_ctx: ProcessingContext, files: &FileProcessor) {
         .filter(SubtitleFile::is_text)
         .process_context(proc_ctx, "Check subtitles")
         .for_each(|(ctx, path)| {
-            let res = check_text_subs(&ctx.borrow(), &path);
-            if let Err(err) = res {
-                ctx.borrow_mut().report_err(err.into());
+            if false {
+                let res = check_text_subs(&ctx.borrow(), &path);
+                if let Err(err) = res {
+                    ctx.borrow_mut().report_err(err.into());
+                }
+            } else {
+                let result = text_subs_fixup(&ctx.borrow(), &path);
+                if let Err(err) = result {
+                    println!("check_sub: {err:#?}");
+                }
             }
             //TODO: handling errors
         });
@@ -131,6 +145,113 @@ where
     });
 
     report
+}
+
+fn text_subs_fixup(proc_ctx: &ProcessingContext, file: &SubtitleFile) -> Result<(), CheckError> {
+    let filename = file.path();
+    proc_ctx.create_sub_process(format!("Check {filename:?}"));
+
+    // let file = File::open(filename).map_err(|source| CheckError::OpeningFile {
+    //     source,
+    //     file: filename.into(),
+    // })?;
+
+    let subs =
+        Subtitles::parse_from_file(filename, None).map_err(|source| CheckError::ParseSrt {
+            source,
+            file: filename.into(),
+        })?;
+    // let filereader = BufReader::new(file);
+    // let lines = filereader.lines();
+    //TODO: add other/configurable check
+
+    let (subtitles, report) = basic_subline_fixup(subs);
+    if report.is_empty() {
+        Ok(())
+    } else {
+        let subtitles = Subtitles::new_from_vec(subtitles);
+        let mut out_filename = file.path().to_path_buf();
+        out_filename.set_extension("fixed.srt");
+        subtitles
+            .write_to_file(&out_filename, None)
+            .map_err(|source| CheckError::WriteToFile {
+                source,
+                file: out_filename,
+            })?;
+
+        Err(CheckError::Report(report))
+    }
+}
+
+//TODO: impl Iterator<Item = String> / use Cow<_, str> ?
+fn basic_subline_fixup<Si>(sublines: Si) -> (Vec<Subtitle>, Report)
+where
+    Si: IntoIterator<Item = Subtitle>, //TODO: use typed Item (with contain but also info like idx, line number, etc)
+{
+    let report = Rc::new(RefCell::new(Report::new()));
+    let sublines = sublines
+        .into_iter()
+        .filter_map(|subline| {
+            if subline.text.is_empty() {
+                report
+                    .borrow_mut()
+                    .push(format!("remove sub {} as empty", subline.num)); //TODO: display timing
+                None
+            } else {
+                Some(subline)
+            }
+        })
+        .enumerate()
+        .map(|(idx, subline)| {
+            let transform = subline.text.lines().map(|line| {
+                if line.starts_with(char::is_whitespace) {
+                    report
+                        .borrow_mut()
+                        .push(format!("sub {idx} start with withespace character"));
+                }
+                if line.ends_with(char::is_whitespace) {
+                    report
+                        .borrow_mut()
+                        .push(format!("sub {idx} end with withespace character"));
+                }
+                let new_line = line.trim().to_string();
+
+                //Check with regex
+                //TODO: only for french
+                static CA_CEDILLE: LazyLock<Regex> =
+                    LazyLock::new(|| Regex::new("^Ca (.*)").unwrap());
+
+                if CA_CEDILLE.is_match(line) {
+                    report
+                        .borrow_mut()
+                        .push(format!("sub {idx} have `Ca` instead of `Ça`"));
+                }
+
+                static CA_REPLACE: LazyLock<Regex> = LazyLock::new(|| Regex::new("Ca ").unwrap());
+                let new_line = CA_REPLACE.replace(new_line.as_ref(), "Ça ");
+
+                static IL_REPLACE: LazyLock<Regex> = LazyLock::new(|| Regex::new("II ").unwrap());
+                let new_line = IL_REPLACE.replace(new_line.as_ref(), "Il ");
+
+                static ILS_REPLACE: LazyLock<Regex> = LazyLock::new(|| Regex::new("IIs ").unwrap());
+                let new_line = ILS_REPLACE.replace(new_line.as_ref(), "Ils ");
+
+                new_line.to_string()
+            });
+
+            let new_text = transform.fold(String::new(), |prev, line| {
+                if prev.is_empty() {
+                    line
+                } else {
+                    format!("{prev}\n{line}")
+                }
+            });
+
+            Subtitle::new(idx, subline.start_time, subline.end_time, new_text)
+        })
+        .collect::<Vec<_>>();
+
+    (sublines, report.take())
 }
 
 #[cfg(test)]
